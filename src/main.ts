7485152d301d97b992be4f491f85541818a9b3de
kami lel <77594253+kami-lel@users.noreply.github.com>
2024-10-23 19:01:42 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,36 @@
 import "./style.css";
 
-const APP_NAME = "HEY THIS IS GAME NAME";
+
+
+
+
+// configs
+const APP_NAME = "Simple Painter"
+const CANVAS_DIMENSION = {x: 256, y: 256}
+
+
+
+
+
+// app
+document.title = APP_NAME;
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
-document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+
+
+
+
+// title element
+const title_element = document.createElement('h1');
+title_element.innerText = APP_NAME;
+app.append(title_element);
+
+
+
+
+
+// canvas element
+const canvas_element = document.createElement('canvas');
+canvas_element.width = CANVAS_DIMENSION.x;
+canvas_element.height = CANVAS_DIMENSION.y;
+app.append(canvas_element);
