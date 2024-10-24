8f7922cda4ee4cb33bef2c2f77b2178e0affba47
kami lel <77594253+kami-lel@users.noreply.github.com>
2024-10-23 18:41:59 -0700

    small change to app name

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import "./style.css";
 
-const APP_NAME = "Hello";
+const APP_NAME = "HEY THIS IS GAME NAME";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
